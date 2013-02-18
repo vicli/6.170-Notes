7c35bca8e8d9eb0d07d88430464ef7a678ee455b
Adrian Mullings <adralemechk@gmail.com>
2013-02-17 19:08:15 -0500

    -Initial commit -Made necessary modifications to run on Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MullingsProj1::Application.config.secret_token = 'e3127f16e47bb36076d93c973d0879213a0672847325742824af57f6a3434abe8f20d627159d8826ae792d51c4389364693e0ce46d72296c04b807cadba3a0a0'
