93af64d44b47c8fe5a969583131e8b153f46e81c
Suthee Ruangwises <suthee@mit.edu>
2013-02-18 23:32:16 -0500

    -

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SutheeProj1::Application.config.secret_token = 'a292304b8be918f61361c30d4af4248bb4cf1d3c3d02d1c03b743e2f610dbfd5b024f935995520efce841efcb0d37b827bf256a28fd7a5c5f2cf7576e7ef5d2e'
