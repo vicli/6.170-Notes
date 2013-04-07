2f188b8e901b61b4427dbd4a0a997a18133d67a5
Victoria Li <victoli.d@gmail.com>
2013-04-07 15:42:09 -0400

    we

--- projectzero/app/controllers/hello_controller.rb
+++
@@ -1,4 +0,0 @@
-class HelloController < ApplicationController
-  def sayhi
-  end
-end
--- projectzero/test/functional/hello_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HelloControllerTest < ActionController::TestCase
-  test "should get sayhi" do
-    get :sayhi
-    assert_response :success
-  end
-
-end
