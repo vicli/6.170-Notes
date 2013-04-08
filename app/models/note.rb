2e2c49759d6c304ac50b00a33bd7a38b98a3f5f5
Victoria Li <victoli.d@gmail.com>
2013-04-07 23:49:47 -0400

    Taking slack day

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ActiveRecord::Base
-  attr_accessible :date_created, :description, :due_date, :name, :owner, :starred, :tag
+  attr_accessible :description, :due_date, :name, :owner, :starred, :tag
   belongs_to :user
 
   validates :name, :presence => true
